2f980d2ed3c8b5995411c5cfc02f56235bc88900
HighOnMelatonin <74775990+HighOnMelatonin@users.noreply.github.com>
2024-02-16 04:10:51 +0000

    hello world

--- jake-challenge/testing-water/app.js
+++ jake-challenge/testing-water/app.js
@@ -1,3 +1,20 @@
+//Hello world
+//example from vscode.com
 var msg = "hello world";
 console.log(msg);
 
+//example from nodejs.org
+const http = require("node:http");
+
+const hostname = '127.0.0.1'
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at https://${hostname}:${port}/`);
+});

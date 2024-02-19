48d5451565da9abd853ee071d908a9a9a7f3312c
HighOnMelatonin <74775990+HighOnMelatonin@users.noreply.github.com>
2024-02-19 04:08:23 +0000

    json package via npm init and npm update

--- README.md
+++ README.md
@@ -48,3 +48,4 @@ A personal website
 
 ### Tech Stack:
 Node.js
+React
--- jake-challenge/testing-water/app.js
+++ jake-challenge/testing-water/app.js
@@ -4,17 +4,22 @@ var msg = "hello world";
 console.log(msg);
 
 //example from nodejs.org
-const http = require("node:http");
+import http from 'node:http';
 
 const hostname = '127.0.0.1'
 const port = 3000;
 
+//request and response, statusCode 200 indicates successful response
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    res.end('Hello World\n');
+    res.writeHead(200, {'Content-Type':'application/json'});
+    res.end(JSON.stringify({
+        data:'Hellow World!',
+    }));
+    //respond with "Hello world"
 });
 
+//server listens at specified port and host name
 server.listen(port, hostname, () => {
     console.log(`Server running at https://${hostname}:${port}/`);
 });
+//To run, run node app.js in the terminal

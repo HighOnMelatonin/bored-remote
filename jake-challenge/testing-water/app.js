8354e5f23b790bd4a1ba3aab9485a955e22994dc
HighOnMelatonin <74775990+HighOnMelatonin@users.noreply.github.com>
2024-02-19 04:27:45 +0000

    reaction commit

--- jake-challenge/testing-water/app.js
+++ jake-challenge/testing-water/app.js
@@ -1,20 +1,20 @@
-//Hello world
+//Hello world using ESM
 //example from vscode.com
 var msg = "hello world";
 console.log(msg);
 
 //example from nodejs.org
 import http from 'node:http';
+//To load ES module, set "type": "module" in the package.json
+//package.json stores project metadata
 
 const hostname = '127.0.0.1'
 const port = 3000;
 
 //request and response, statusCode 200 indicates successful response
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type':'application/json'});
-    res.end(JSON.stringify({
-        data:'Hellow World!',
-    }));
+    res.writeHead(200, {'Content-Type':'plain text'});
+    res.end('Hello World!');
     //respond with "Hello world"
 });
 

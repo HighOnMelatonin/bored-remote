aa6f22742ba2938954b3154a0c9201387130b68f
HighOnMelatonin <74775990+HighOnMelatonin@users.noreply.github.com>
2024-02-20 02:45:47 +0000

    hello express

--- jake-challenge/testing-water/expression.js
+++ jake-challenge/testing-water/expression.js
@@ -1,9 +1,13 @@
 //Using express
 
-import express from 'node:express';
+import express from 'express';
 
 const port = process.env.port || 3001;
 const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello World')
+});
 
 app.listen(port, () => {
     console.log(`Server listening on ${port}`)

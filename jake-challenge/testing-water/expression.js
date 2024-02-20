8482c7a2560a8b0efaae8d768dd6666077b70cd2
HighOnMelatonin <74775990+HighOnMelatonin@users.noreply.github.com>
2024-02-20 07:05:48 +0000

    Revert "hello express"
    This reverts commit aa6f22742ba2938954b3154a0c9201387130b68f.

--- jake-challenge/testing-water/expression.js
+++ jake-challenge/testing-water/expression.js
@@ -1,17 +1,24 @@
 //Using express
 
+<<<<<<< HEAD
 import express from 'express';
 import path from 'path';
+=======
+import express from 'node:express';
+>>>>>>> parent of aa6f227 (hello express)
 
 const port = process.env.port || 3001;
 const app = express();
 
+<<<<<<< HEAD
 //this is a response to a get request
 app.get('/', (req,res) => {
     app.use(express.static('reaction'))
     //display static objects, express.static('directory_name')
 });
 
+=======
+>>>>>>> parent of aa6f227 (hello express)
 app.listen(port, () => {
     console.log(`Server listening on ${port}`)
 });

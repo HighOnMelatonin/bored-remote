205451ebd2c44051acb48b88fb46f4ad9a31a9a8
HighOnMelatonin <74775990+HighOnMelatonin@users.noreply.github.com>
2024-02-22 02:27:01 +0000

    added html as an attempt to get express working

--- jake-challenge/testing-water/expression.js
+++ jake-challenge/testing-water/expression.js
@@ -3,12 +3,12 @@
 import express from 'express';
 import path from 'path';
 
-const port = process.env.port || 3001;
+const port = process.env.port || 3000;
 const app = express();
 
 //this is a response to a get request
 app.get('/', (req,res) => {
-    app.use('/',express.static(path.join('app.js','reaction')))
+    app.use('/',express.static(path.join('home.html','reaction')))
     //display static objects, express.static('directory_name')
 });
 

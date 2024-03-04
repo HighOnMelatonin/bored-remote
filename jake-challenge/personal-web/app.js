6afebacfd281f57b889f4c16ac0f67addbc755fe
HighOnMelatonin <tanruianh@outlook.com>
2024-03-04 09:05:57 +0800

    beginnings of a website

--- jake-challenge/personal-web/app.js
+++ jake-challenge/personal-web/app.js
@@ -1,1 +1,12 @@
 //Main program
+//personal webpage
+import http from "node:http";
+
+const hostname = '127.0.0.1';
+const port = '9000';
+
+const server = http.createServer((req, res) => {
+    
+})
+
+server.listen(port, hostname)
